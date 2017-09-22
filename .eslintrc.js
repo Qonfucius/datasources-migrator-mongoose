e81ce87dc64d86609c064da53260ee4e961617ef
Gaël <gael@hungryup.co>
2017-09-22 17:17:58 +0200

    ✨ Adding exceptions to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,10 @@ module.exports = {
   rules: {
     'max-len': ['error', 120],
     'func-names': 0,
+    'no-bitwise': 0,
+    'no-await-in-loop': 0,
+    'import/no-dynamic-require': 0,
+    'global-require': 0,
+    'no-restricted-syntax': 0,
   },
 };
