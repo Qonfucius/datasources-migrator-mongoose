45af4c419eb19bca0e1d93b30c47c9523b7165ba
Gaël <gael@hungryup.co>
2017-09-22 12:51:01 +0200

    ✨ Split module & bin + refactor config

--- index.js
+++ index.js
@@ -4,36 +4,39 @@ const fs = require('fs');
 const { join } = require('path');
 const readdir = promisify(fs.readdir);
 
-module.exports = {
-  async play(db, { path, rollback }, migrationFiles = []) {
-    const migrations = migrationFiles
-      .filter(file => ~file.search(/^[^.].*\.js$/))
-      .map(file => require(join(process.cwd(), path, file)));
-    if (rollback) {
-      migrations.reverse();
-    }
-    for (const migration of migrations) {
-      await migration[rollback ? 'down' : 'up'](mongoose);
-    }
-  },
+module.exports = function ({ path, rollback, uri, models, destroyOld = false }, workingDir) {
+  return {
+    async play(db, migrationFiles = []) {
+      const migrations = migrationFiles
+        .filter(file => ~file.search(/^[^.].*\.js$/))
+        .map(file => require(join(workingDir, path, file)));
+      if (rollback) {
+        migrations.reverse();
+      }
+      for (const migration of migrations) {
+        await migration[rollback ? 'down' : 'up'](mongoose);
+      }
+    },
 
-  async init({ uri, models, destroyOld = false } = {}) {
-    mongoose.Promise = Promise;
-    mongoose.connection.on('error', console.error);
-    const db = await mongoose.connect(uri);
-    if (destroyOld) {
-      await mongoose.connection.db.dropDatabase();
-    }
-    const modelsPath = join(process.cwd(), models);
-    (await readdir(modelsPath))
-      .filter(file => ~file.search(/^[^.].*\.js$/))
-      .map(modelName => require(join(modelsPath, modelName)))
-      .filter(model => !!model.schema)
-      .forEach(model => mongoose.model(model.modelName, model.schema));
-    return db;
-  },
+    async init() {
+      mongoose.Promise = Promise;
+      mongoose.connection.on('error', console.error);
+      const db = await mongoose.connect(uri);
+      if (destroyOld) {
+        await mongoose.connection.db.dropDatabase();
+      }
+      const modelsPath = join(workingDir, models);
+      console.log(modelsPath);
+      (await readdir(modelsPath))
+        .filter(file => ~file.search(/^[^.].*\.js$/))
+        .map(modelName => require(join(modelsPath, modelName)))
+        .filter(model => !!model.schema)
+        .forEach(model => mongoose.model(model.modelName, model.schema));
+      return db;
+    },
 
-  async end() {
-    return mongoose.disconnect();
-  }
+    async end() {
+      return mongoose.disconnect();
+    }
+  };
 };
